6fdfff5ce9f26e6159d6820e512cfdaf6380b242
Andrew Dacenko <a.s.dvornik@gmail.com>
2015-02-18 15:13:03 +0200

    Third version

--- js/main.js
+++ js/main.js
@@ -1,37 +1,59 @@
-var data = [4, 8, 15, 16, 23, 42];
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-var width = 420,
-    barHeight = 20;
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
 
-var x = d3.scale.linear()
-    .range([0, width]);
+var y = d3.scale.linear()
+    .range([height, 0]);
 
-var chart = d3.select(".chart")
-    .attr("width", width);
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
 
-// d3.tsv("data/d3.tsv", type, function(error, data) {
-d3.json("data/d3.json", function(error, data) {
-  x.domain([0, d3.max(data, function(d) { return d.value; })]);
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10, "%");
 
-  chart.attr("height", barHeight * data.length);
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  var bar = chart.selectAll("g")
+d3.tsv("data/d3.tsv", type, function(error, data) {
+  x.domain(data.map(function(d) { return d.letter; }));
+  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Frequency");
+
+  svg.selectAll(".bar")
       .data(data)
-    .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.letter); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.frequency); })
+      .attr("height", function(d) { return height - y(d.frequency); });
 
-  bar.append("rect")
-      .attr("width", function(d) { return x(d.value); })
-      .attr("height", barHeight - 1);
-
-  bar.append("text")
-      .attr("x", function(d) { return x(d.value) - 3; })
-      .attr("y", barHeight / 2)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.value; });
 });
 
-// function type(d) {
-//   d.value = +d.value; // coerce to number
-//   return d;
-// }
+function type(d) {
+  d.frequency = +d.frequency;
+  return d;
+}
